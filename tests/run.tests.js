aa3f9d47f8dbb7b34d0221ac9b1994f12696ad2f
zaphod1984 <zaphod84@gmx.de>
2013-07-16 10:01:01 +0200

    improved tests

--- tests/run.tests.js
+++ tests/run.tests.js
@@ -11,15 +11,13 @@ describe('Run', function() {
 
         var myRun = new Run(moment().add('seconds', 10));
 
-        var executed = false;
-        myRun.execute(function() {
-            executed = true;
-        });
+        var executionSpy = sinon.spy();
+        myRun.execute(executionSpy);
 
         //20s in the future
         clock.tick(20000);
 
-        expect(executed).to.be.true;
+        expect(executionSpy.callCount).to.equal(1);
 
         clock.restore();
     });
@@ -29,15 +27,15 @@ describe('Run', function() {
 
         var myRun = new Run(moment().add('seconds', 10));
 
-        var executed = false;
-        myRun.execute(function() { executed = true; });
+        var executionSpy = sinon.spy();
+        myRun.execute(executionSpy);
 
         myRun.clear();
 
         //20s in the future
         clock.tick(20000);
 
-        expect(executed).to.be.false;
+        expect(executionSpy.callCount).to.equal(0);
 
         clock.restore();
     });
@@ -59,7 +57,7 @@ describe('Run', function() {
         //20s in the future
         clock.tick(20000);
 
-        //0s   5s   10S  15s
+        //0s   5s   10s  15s
         //-----S----S-----X
 
         expect(executionSpy.callCount).to.equal(1);
